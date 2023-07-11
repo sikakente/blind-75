42e2a5434974752a45e32a6e21d2dd90e2a5c882
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2023-07-11 10:11:13 +0100

    Closes #26. Solves linked list cycle

---
+++ linked_lists/has_cycle.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+
+        if not head:
+            return False
+
+        fast = slow = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast is slow:
+                return True
+
+        return False
