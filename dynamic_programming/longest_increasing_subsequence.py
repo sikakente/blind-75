1ec9d9874b2aa3a4ccb95a576cd9d6769cace4c2
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2023-07-11 10:43:40 +0100

    Closes #108. Solves longest increasing subsequence

--- dynamic_programming/longest_increasing_subsequence.py
+++ dynamic_programming/longest_increasing_subsequence.py
@@ -22,3 +22,46 @@ class Solution:
             return cache[(i, prev)]
 
         return lis(0, float("-inf"))
+
+
+class Solution2:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 1:
+            return n
+        cache = {}
+
+        def helper(idx, prev):
+            if (idx, prev) in cache:
+                return cache[(idx, prev)]
+            if idx == n:
+                return 0
+
+            including = 0
+            if nums[idx] > prev:
+                including = 1 + helper(idx + 1, nums[idx])
+
+            excluding = helper(idx + 1, prev)
+            cache[(idx, prev)] = max(including, excluding)
+            return cache[(idx, prev)]
+
+        return helper(0, float("-inf"))
+
+
+class Solution3:
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        N = len(nums)
+        nums = [0] + nums
+        memo = [1 for _ in range(N + 1)]
+        memo[0] = 0
+        lis = float("-inf")
+
+        for i in range(1, N + 1):
+            current_num = nums[i]
+            for j in range(1, i):
+                compared_num = nums[j]
+                if compared_num < current_num and 1 + memo[j] > memo[i]:
+                    memo[i] = max(memo[i], 1 + memo[j])
+
+        return max(memo)
