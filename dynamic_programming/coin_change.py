1c56f5efb1838744b08c45d77b2fc1670284d095
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2023-07-11 11:23:04 +0100

    Closes #109. Solves coin change problem

--- dynamic_programming/coin_change.py
+++ dynamic_programming/coin_change.py
@@ -28,3 +28,20 @@ class Solution:
 
         ans = change(0, amount)
         return -1 if ans == float("inf") else ans
+
+
+class Solution2:
+
+    def coin_change(self, coins: List[int], amount: int) -> int:
+        denoms = coins
+        n = amount
+        all_change = [float("inf")] * (n + 1)
+        all_change[0] = 0
+
+        for target in range(1, n + 1):
+            for denom in denoms:
+                difference = target - denom
+                if difference >= 0:
+                    all_change[target] = min(all_change[difference] + 1, all_change[target])
+
+        return all_change[n] if all_change[n] != float("inf") else -1
