f9f97ac1136b06e5f03772d5c6c40b1f245bf5f6
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2023-07-31 19:20:19 +0100

    Closes #71. Best time to buy and sell stocks

--- arrays/best_time_to_buy_and_sell_stock.py
+++ arrays/best_time_to_buy_and_sell_stock.py
@@ -13,3 +13,14 @@ class Solution:
 
         return profit
 
+
+class Solution2:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        n = len(prices)
+
+        for i in range(1, n):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+
+        return profit
